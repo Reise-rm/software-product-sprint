a89e55fa39fc4cd3ac3ad8561b611a560eec2fdd
Reise-rm <1700012716@pku.edu.cn>
2020-07-11 00:10:54 -0400

    Add contents to my portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Travelling', 'Driving', 'Reading Books', 'Watching Movies', 'Listening to Music', 'Singing Songs', 'Writing Thoughts', 'Taking Photos', 'Wondering'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -25,4 +25,36 @@ function addRandomGreeting() {
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+
+
+/**
+ * Adds a random singer to the page.
+ */
+function addRandomSingers() {
+  const singers =
+      ['Jack Johonson', 'Bruno Major', 'Adele', 'Sam Smith', 'Billie Eilish', '陈粒', '郭顶', '许嵩', '周杰伦', '余佳运', '李荣浩', '落日飞车', '橘子海'];
+
+  // Pick a random singer.
+  const singer = singers[Math.floor(Math.random() * singers.length)];
+
+  // Add it to the page.
+  const singerContainer = document.getElementById('singer-container');
+  singerContainer.innerText = singer;
+}
+
+
+/**
+ * Adds a random story to the page.
+ */
+function addRandomStories() {
+  const stories =
+      ['一代宗师', '白日焰火', '少年的你', '白夜行', '地球最后的夜晚', 'Jason Bourne', 'Prison Break', 'Green Book', 'Everything I Never Told You', 'Her', 'The Long Goodbye', 'Fleabag', '乌有园', '红色沙漠', 'The End of The xx World'];
+
+  // Pick a random singer.
+  const story = stories[Math.floor(Math.random() * stories.length)];
+
+  // Add it to the page.
+  const storyContainer = document.getElementById('story-container');
+  storyContainer.innerText = story;
 }
